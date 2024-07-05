ee84271286c2b9e88bb8d157774194c34cc1f914
jasonzhang <jasonzhang@zego.im>
2024-07-05 14:48:27 +0800

    change: 修改了appid

--- iOS/APIExample-OC/APIExample-OC/Common/KeyCenter.m
+++ iOS/APIExample-OC/APIExample-OC/Common/KeyCenter.m
@@ -7,7 +7,10 @@
 
 #import "KeyCenter.h"
 
-static NSString * const APPID = <#YOUR APPID#>
+//static NSString * const APPID = @"abc54617574441fbb42ce41f57c044fe";
+//static NSString * const Certificate = @"d99a513117264109987ed3340c495e78";
+//static NSString * const APPID = @"6594b763a7e84a2d89350d1a7749d99d";
+static NSString * const APPID = @"e8f38ee7283e4f6eaed4ecec6fcc5b3a";
 static NSString * const Certificate = nil;
 
 @implementation KeyCenter
